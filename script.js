45901789b0b3992d47332b237a11cc7714ea04b8
devesh-69 <tatkaredevesh69@gmail.com>
2024-07-12 12:38:26 +0530

    Updated Now

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 //     el: document.querySelector('#main'),
 //     smooth: true
 // });
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    stagePadding: 70,
+    nav: false,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        },
+        1440: {
+            items: 5
+        }
+    }
+})
