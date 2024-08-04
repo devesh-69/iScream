a464d45d42b1fec1e5d1dc3a6a66b7a21cdb4cbe
devesh-69 <tatkaredevesh69@gmail.com>
2024-08-04 19:09:13 +0530

    update

--- specialOffers/factories.js
+++ specialOffers/factories.js
@@ -1,6 +1,36 @@
 
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 function booknow() {
     // Scroll to the element with id 'container5'
     document.getElementById('book-formNow').scrollIntoView({ behavior: 'smooth' });
 }
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 18,
+    stagePadding: 40,
+    nav: false,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        },
+        1440: {
+            items: 3
+        }
+    }
+})
